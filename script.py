f65254a56b2616385302f4dfd9a0d02e7c130225
Rud Maxim <makru003@mail.ru>
2023-07-02 22:32:15 +0300

    initial commit

---
+++ script.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as chromeService
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = Options()
+options.add_argument("--window-size=1920,800")
+options.add_argument("--headless")
+
+driver = webdriver.chrome(service=chromeService(GeckoDriverManager().install()), options=options)
+
+driver.implicitly_wait(10)
+
+# Константы
+URL = "https://www.saucedemo.com/"
+LOGIN = "standard_user"
+PASSWORD = "secret_sauce"
+
+driver.get(URL)
+
+# Заполнение полей логина и пароля
+input_login = driver.find_element(By.ID, "user-name")
+input_password = driver.find_element(By.ID, "password")
+input_login.send_keys(LOGIN)
+input_password.send_keys(PASSWORD)
+
+# Нажатие кнопки логина
+login_page_button = driver.find_element(By.ID, "login-button")
+login_page_button.click()
+
+driver.quit()
